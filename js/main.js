7341eb658e28cdcfa980bb312970eba41f2fc2e7
xaca <and1res@hotmail.com>
2019-07-09 17:42:25 -0500

    Empiezo a integrar el recorrido en video con los diferentes estados de interaccion

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let indice = 1;
+let indice = 2;
 let fin_escena = false;
 let interval_id;
 let video_actual;
@@ -8,17 +8,22 @@ let boton_ventana_pachamama, boton_cerrar_ventana;
 let secciones = [];
 
 const videos = [];
-videos[1] =  {c:"g1", id:1,v:"UJrPam0C5Vk",d:false,s:2};
-videos[2] =  {c:"g2", id:2,v:"J_USarY7szs",d:false,s:3};
-videos[3] =  {c:"g3", id:3,v:"TJ4hQjqDrOU",d:false,s:4};
-videos[4] =  {c:"g4", id:4,v:"7kOPn7uFfn8",d:true,s:[5,6,7]};
-videos[5] =  {c:"g5a",id:5,v:"78eBaDqOCVc",d:false,s:[]};
-videos[6] =  {c:"g5b",id:6,v:"QFMglskQsDI",d:false,s:[]};
-videos[7] =  {c:"g6", id:7,v:"5zgk1mvyVN0",d:true,s:[9,10,8]};
-videos[8] =  {c:"g6a",id:8,v:"qoI2zUSwRos",d:false,s:[]};
-videos[9] =  {c:"g6b",id:9,v:"4iFtrOVXJjw",d:false,s:[]};
-videos[10] = {c:"g7", id:10,v:"UgUg0fT8Rvw",d:false,s:11};
-videos[11] = {c:"g8", id:11,v:"n1EuIINYmTk",d:false,s:12};
+/*
+c ->Id de la imagen de fondo que se carga al final del video
+s ->Siguiente video
+v ->Id del video de youtube
+*/
+videos[2] =  {c:"g2", id:1,v:"UJrPam0C5Vk",d:false,s:3};
+videos[3] =  {c:"g2", id:2,v:"J_USarY7szs",d:false,s:4};
+videos[4] =  {c:"g3", id:3,v:"TJ4hQjqDrOU",d:false,s:5};
+videos[5] =  {c:"g4", id:4,v:"7kOPn7uFfn8",d:false,s:6};
+videos[6] =  {c:"g5a",id:5,v:"78eBaDqOCVc",d:true,s:[7,8,9]};
+videos[7] =  {c:"g5b",id:6,v:"QFMglskQsDI",d:false,s:[]};
+videos[8] =  {c:"g6", id:7,v:"5zgk1mvyVN0",d:true,s:[9,10,8]};
+videos[9] =  {c:"g6a",id:8,v:"qoI2zUSwRos",d:false,s:[]};
+videos[10] =  {c:"g6b",id:9,v:"4iFtrOVXJjw",d:false,s:[]};
+videos[11] = {c:"g7", id:10,v:"UgUg0fT8Rvw",d:false,s:11};
+videos[12] = {c:"g8", id:11,v:"n1EuIINYmTk",d:false,s:12};
 //videos[12] = {c:"g9", id:12,v:"fUfDBsZTK1g",d:false,s:-1};//Indica el fin
 
 // 2. This code loads the IFrame Player API code asynchronously.
@@ -60,32 +65,21 @@ window.onload = function()
 
 function asignarReferencias()
 { 
-    secciones[0] = undefined;
-    secciones.push(document.getElementById("seccion_01"));
-    secciones.push(document.getElementById("seccion_02"));
-    secciones.push(document.getElementById("seccion_03"));
-    secciones.push(document.getElementById("seccion_04"));
-    secciones.push(document.getElementById("seccion_05"));
-    secciones.push(document.getElementById("seccion_06"));
-    secciones.push(document.getElementById("seccion_07"));
-    secciones.push(document.getElementById("seccion_08"));
-    secciones.push(document.getElementById("seccion_09"));
-    secciones.push(document.getElementById("seccion_10"));
-    secciones.push(document.getElementById("seccion_11"));
-    secciones.push(document.getElementById("seccion_12"));
+    secciones[1] = document.getElementById("seccion_01");
+    secciones[2] = document.getElementById("seccion_02");
+    secciones[3] = document.getElementById("seccion_03");
+    secciones[4] = document.getElementById("seccion_04");
+    secciones[5] = document.getElementById("seccion_05");
+    secciones[6] = document.getElementById("seccion_06");
+    secciones[7] = document.getElementById("seccion_07");
+    secciones[8] = document.getElementById("seccion_08");
+    secciones[9] = document.getElementById("seccion_09");
+    secciones[10] = document.getElementById("seccion_10");
+    secciones[11] = document.getElementById("seccion_11");
+    secciones[12] = document.getElementById("seccion_12");
     control_progreso = document.getElementById("control_progreso");
     boton_ventana_pachamama = document.getElementById("boton_ventana_pachamama");
     boton_cerrar_ventana = document.getElementById("boton_cerrar_ventana");
-}
-function ocultarSeccion(){
-    for (let i=1;i<secciones.length;i++) {
-        secciones[i].className = "contenedor ocultar";
-    }
-}
-function cargarSeccion(id){
-   ocultarSeccion();
-   console.log(id);
-   secciones[id].className = "contenedor"; 
 }
 function agregarEventos()
 {
@@ -130,6 +124,11 @@ function arriba(){
     ocultarMensaje();
 }
 
+function obtenerId(value)
+{
+    return  value.split("btn")[1];
+}
+
 function quitarSeleccion()
 {
     let botones = document.getElementsByClassName("boton_avance_circular");
@@ -138,30 +137,34 @@ function quitarSeleccion()
         botones[key].className = "boton_avance_circular btn"+cont++;
     }
 }
-
-function obtenerId(value)
-{
-    return  value.split("btn")[1];
-}
-
 function actualizarProgreso(indice, target)
 {
+    let id_simple = obtenerId(target.id);
     quitarSeleccion();
     seleccionarBoton(target.id);
-    cargarSeccion(obtenerId(target.id));
+    cargarSeccion(id_simple);
+    cargarVideo(id_simple);
     control_progreso.className = "sprite progreso_"+indice;
 }
-
+function ocultarSeccion(){
+    for (let i=1;i<secciones.length;i++) {
+        secciones[i].className = "contenedor ocultar";
+    }
+}
+function cargarSeccion(id){
+   ocultarSeccion();
+   secciones[id].className = "contenedor"; 
+}
 function seleccionarBoton(id)
 {
     document.getElementById(id).className = "boton_avance_circular seleccionado "+id;
 }
 
 function cargarVideo(indice){
-    
-    /*if(indice>0){
+    console.log(indice)
+    if(indice>0){
         
-        ocultarMensaje();
+        //ocultarMensaje();
         fin_escena = false;
         video_actual = videos[indice];            
         player.loadVideoById(video_actual.v, 0, "default");
@@ -169,22 +172,23 @@ function cargarVideo(indice){
     else{
         detenerVideo();
         alert("fin de la experiencia :)");
-    }*/
+    }
 }
 
 function onPlayerStateChange(event) {
-   /*  if (event.data == YT.PlayerState.PLAYING) {
-        sound.play();
+    if (event.data == YT.PlayerState.PLAYING) {
+        //sound.play();
     }
     if(event.data == YT.PlayerState.PAUSED)
     {
-        sound.pause();
+        //sound.pause();
     }
     if(event.data == YT.PlayerState.ENDED){
-        sound.pause();
-       
+        //sound.pause();
+       console.log(video_actual);
         if(Array.isArray(video_actual.s))
         {
+            /*
             let controles_config = "";
             if(video_actual.id == 4 || video_actual.id == 5 || video_actual.id == 6)
             {
@@ -197,21 +201,21 @@ function onPlayerStateChange(event) {
                 video_actual = videos[7];
                 controles_config = "i,d,a";
             }
-
-            mostrarMensaje({
+            */
+            /*mostrarMensaje({
                 estado:"CONDICIONAL",
                 controles:controles_config,
                 imagen:"img/"+video_actual.c+".jpg"
-            });
+            });*/
         }
         else
         {
-            mostrarMensaje({
+           mostrarMensaje({
                 estado:"FIN_VIDEO",
                 imagen:"img/"+video_actual.c+".jpg"
-            });
+            }); 
         }
-    } */
+    }
 }
 
 function ocultarMensaje()
@@ -234,7 +238,7 @@ function mostrarMensaje(obj)
 
     if(obj.estado == "FIN_VIDEO")
     {
-        cargarPlay(video_actual.s);
+        //cargarPlay(video_actual.s);
         cargarImagen(obj.imagen);
     }
 }
